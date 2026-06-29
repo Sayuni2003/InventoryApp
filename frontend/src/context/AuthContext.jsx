2c3a58a3fe7edef2e5104690996b61e98f74deb5
Sayuni2003 <sayuni3manjusri@gmail.com>
2026-06-30 00:16:00 +0530

    Revert "add health route"
    This reverts commit a87644ea39effd60c235df25d240f591d8f57f67.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -23,8 +23,6 @@ export const AuthProvider = ({ children }) => {
         const token = await fbUser.getIdToken();
 
         try {
-          await fetch("https://inventory-app-7nrq.onrender.com/health");
-
           const res = await axiosInstance.get("/api/auth/me", {
             headers: { Authorization: `Bearer ${token}` },
           });
